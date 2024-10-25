a4588ce704887bb1cc9eeadd44eee956bd2e4864
Matej Pacan <matej@matejpacan.com>
2024-10-25 09:32:25 +0200

    Add new MC attributes

--- foundation-bukkit/src/main/java/org/mineacademy/fo/remain/CompAttribute.java
+++ foundation-bukkit/src/main/java/org/mineacademy/fo/remain/CompAttribute.java
@@ -58,6 +58,11 @@ public enum CompAttribute {
 	GENERIC_ATTACK_DAMAGE("generic.attackDamage", "ATTACK_DAMAGE"),
 
 	/**
+	 * Attack knockback of an Entity.
+	 */
+	GENERIC_ATTACK_KNOCKBACK("generic.attack_knockback"),
+
+	/**
 	 * Attack speed of an Entity.
 	 */
 	GENERIC_ATTACK_SPEED("generic.attackSpeed"),
@@ -73,14 +78,104 @@ public enum CompAttribute {
 	GENERIC_ARMOR_TOUGHNESS("generic.armorToughness"),
 
 	/**
+	 * The fall damage multiplier of an Entity.
+	 */
+	GENERIC_FALL_DAMAGE_MULTIPLIER("generic.fall_damage_multiplier"),
+
+	/**
 	 * Luck bonus of an Entity.
 	 */
 	GENERIC_LUCK("generic.luck"),
 
 	/**
+	 * Maximum absorption of an Entity.
+	 */
+	GENERIC_MAX_ABSORPTION("generic.max_absorption"),
+
+	/**
+	 * The distance which an Entity can fall without damage.
+	 */
+	GENERIC_SAFE_FALL_DISTANCE("generic.safe_fall_distance"),
+
+	/**
+	 * The relative scale of an Entity.
+	 */
+	GENERIC_SCALE("generic.scale"),
+
+	/**
+	 * The height which an Entity can walk over.
+	 */
+	GENERIC_STEP_HEIGHT("generic.step_height"),
+
+	/**
+	 * The gravity applied to an Entity.
+	 */
+	GENERIC_GRAVITY("generic.gravity"),
+
+	/**
 	 * Strength with which a horse will jump.
 	 */
 	HORSE_JUMP_STRENGTH("horse.jumpStrength"),
+
+	/**
+	 * How long an entity remains burning after ingition.
+	 */
+	GENERIC_BURNING_TIME("generic.burning_time"),
+
+	/**
+	 * Resistance to knockback from explosions.
+	 */
+	GENERIC_EXPLOSION_KNOCKBACK_RESISTANCE("generic.explosion_knockback_resistance"),
+
+	/**
+	 * Movement speed through difficult terrain.
+	 */
+	GENERIC_MOVEMENT_EFFICIENCY("generic.movement_efficiency"),
+
+	/**
+	 * Oxygen use underwater.
+	 */
+	GENERIC_OXYGEN_BONUS("generic.oxygen_bonus"),
+
+	/**
+	 * Movement speed through water.
+	 */
+	GENERIC_WATER_MOVEMENT_EFFICIENCY("generic.water_movement_efficiency"),
+
+	/**
+	 * The block reach distance of a Player.
+	 */
+	PLAYER_BLOCK_INTERACTION_RANGE("player.block_interaction_range"),
+
+	/**
+	 * The entity reach distance of a Player.
+	 */
+	PLAYER_ENTITY_INTERACTION_RANGE("player.entity_interaction_range"),
+
+	/**
+	 * Block break speed of a Player.
+	 */
+	PLAYER_BLOCK_BREAK_SPEED("player.block_break_speed"),
+
+	/**
+	 * Mining speed for correct tools.
+	 */
+	PLAYER_MINING_EFFICIENCY("player.mining_efficiency"),
+
+	/**
+	 * Sneaking speed.
+	 */
+	PLAYER_SNEAKING_SPEED("player.sneaking_speed"),
+
+	/**
+	 * Underwater mining speed.
+	 */
+	PLAYER_SUBMERGED_MINING_SPEED("player.submerged_mining_speed"),
+
+	/**
+	 * Sweeping damage.
+	 */
+	PLAYER_SWEEPING_DAMAGE_RATIO("player.sweeping_damage_ratio"),
 
 	/**
 	 * Chance of a Zombie to spawn reinforcements.
